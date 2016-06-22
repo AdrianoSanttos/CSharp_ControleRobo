1da6bb363d9f9b1fa988f95ec6fae9276f82912e
Adriano Santos <adrianosanttos1987@hotmail.com>
2016-06-22 17:42:33 -0300

    C#: Controle Robô
    Teste executado para um entrevista de emprego:
    
    Escopo
    Aplicacao para controlar remotamente um robo prospector de petroleo. O
    robo funciona da seguinte maneira:
    - Em um dado momento, o robo se encontra em 1 de 4 estados possiveis:
    o Pronto: O robo pode receber uma nova tarefa e realiza-la
    imediatamente.
    o Ocupado: O robo se encontra executando uma determinada tarefa e não
    pode receber novas tarefas.
    o Recuperando: O robo não esta executando nenhuma tarefa, mas ja pode
    receber a proxima tarefa que ira executar apos o período de recuperacao.
    o Cheio: O compartimento de petroleo do robo esta cheio e o mesmo nao
    esta mais apto para servico. Isto ocorre apos 5 extracoes bem sucedidas
    de petroleo.
    - O robo pode receber diversas tarefas, em especial para este teste, as
    seguintes tarefas sao necessarias:
    o (A)ndar: O robo anda por 15 segundos para uma localidade não
    explorada.
    o (P)rocurar: O robo procura petroleo por 25 segundos no local atual. A
    chance de encontrar petroleo e de 50% apos os 25 segundos.
    o Extrair: O robo extrai petróleo da área atual por 30 segundos. Para
    efeito deste exemplo, uma extração sempre é bem sucedida pois so deve
    ser ordenada apos um sucesso em uma acao de “procurar”.
    - Por limitacoes de projeto, o robo nao e uma entidade inteligente capaz
    de tomar decisoes por si so e, todas estas devem vir de um ou mais
    centros de comando. O software do centro de comando pode receber como
    input um padrao de buscas para enviar para o robo executar. Exemplo:
    o A A P P A A A P - O software enviará para o robô as ordens de “andar”,
    “andar”, “procurar”, “procurar”, “andar”, “andar”, “andar” e “procurar”
    - O software do centro de comando deve ordenar que o robo realize
    extracao de petroleo sempre que uma operacao de buscar o encontrar.
    - A missao do robo deve ser considerada um sucesso se, apos executar o
    script
    de buscas, o mesmo estiver em um estado “Cheio”.
    
    Scope
    Application to remotely control a prospector robo oil. The robot works
    as follows:
    - At one point, the robot is in one of four possible states:
    Ready: The robot can receive a new task and performs it immediately.
    Busy: The robot is performing a certain task and can not receive new
    jobs.
    Recovering: The robot is not executing any task, but can already receive
    the next task that will run after the recovery period.
    the Full: The oil compartment robo is full and it is not this more fit
    for service. This occurs after 5 successful extractions of oil.
    - The robot can receive various tasks, especially for this test, the
    following tasks are necessary:
    (A) ndar: The robot walks for 15 seconds to a location not exploited.
    the (P) rocurar: The robo demand oil for 25 seconds at the current
    location. The chance of finding oil and 50% after 25 seconds.
    the Extract: The robot extracts oil from the current area for 30
    seconds. For purposes of this example, an extraction is always
    successful because only be ordered after a success in an action of
    "search".
    - By design limitations, the robot and not an intelligent entity capable
    of taking decisions by itself and all these must come from one or more
    control centers. The Command Center software can receive as input a
    search pattern to send to the robo run. Example:
    o P P A A P - The software will send the commands to the robot to
    "walk", "walk", "search", "search", "walk", "walk", "walking" and
    "search"
    - The command center software should order the robot to perform
    extraction of oil whenever an operation to search to find it.
    - The mission of the robot should be considered a success if, after
    running the script
    search, it is in a "Full" state.

---
+++ Projeto - Robô/robo/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace robo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAndar = new System.Windows.Forms.Button();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.lblTarefa = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblMsgSucesso = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAndar
+            // 
+            this.btnAndar.Location = new System.Drawing.Point(36, 78);
+            this.btnAndar.Name = "btnAndar";
+            this.btnAndar.Size = new System.Drawing.Size(114, 55);
+            this.btnAndar.TabIndex = 0;
+            this.btnAndar.Text = "Andar";
+            this.btnAndar.UseVisualStyleBackColor = true;
+            this.btnAndar.Click += new System.EventHandler(this.Andar_Click);
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(231, 78);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(114, 55);
+            this.btnProcurar.TabIndex = 1;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.Procurar_Click);
+            // 
+            // lblTarefa
+            // 
+            this.lblTarefa.AutoSize = true;
+            this.lblTarefa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTarefa.Location = new System.Drawing.Point(37, 173);
+            this.lblTarefa.Name = "lblTarefa";
+            this.lblTarefa.Size = new System.Drawing.Size(59, 20);
+            this.lblTarefa.TabIndex = 2;
+            this.lblTarefa.Text = "Tarefa:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::robo.Properties.Resources.rock1__article_image;
+            this.pictureBox2.Location = new System.Drawing.Point(266, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::robo.Properties.Resources.mega_man1;
+            this.pictureBox1.Location = new System.Drawing.Point(62, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(32, 205);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(64, 20);
+            this.lblEstado.TabIndex = 5;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblMsgSucesso
+            // 
+            this.lblMsgSucesso.AutoSize = true;
+            this.lblMsgSucesso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsgSucesso.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblMsgSucesso.Location = new System.Drawing.Point(73, 237);
+            this.lblMsgSucesso.Name = "lblMsgSucesso";
+            this.lblMsgSucesso.Size = new System.Drawing.Size(220, 20);
+            this.lblMsgSucesso.TabIndex = 6;
+            this.lblMsgSucesso.Text = "Sucesso, o Robô está cheio!!!";
+            this.lblMsgSucesso.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 304);
+            this.Controls.Add(this.lblMsgSucesso);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTarefa);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.btnAndar);
+            this.Name = "Form1";
+            this.Text = "Projeto Liera";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAndar;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Label lblTarefa;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblMsgSucesso;
+    }
+}
+
---
+++ Projeto - Robô/robo/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace robo
+{
+    public partial class Form1 : Form
+    {
+        DateTime DataAgora = new DateTime(); //Hora atual
+        DateTime DataEncerra = new DateTime(); //Hora que irá encerrar o processo
+        string Tarefa; //Objeto que recebe a tarefa
+        int contadorExtracao; //objeto contador de extrações
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblEstado.Text = "Estado: Pronto";
+        }
+      
+
+        // Evento do Click do botão Andar
+        private void Andar_Click(object sender, EventArgs e)
+        {
+                lblTarefa.Text = "Tarefa: Andar";
+                Tarefa = "A";
+                DataAgora = DateTime.Now;
+                DataEncerra = DataAgora.AddSeconds(10);  //Adiciona 10 segundos ao tempo atual            
+                timer1.Start();  //Inicia o timer              
+                lblEstado.Text = "Estado: Ocupado ";
+                
+            
+        }
+
+        //Evento do Click do botão Procurar
+        private void Procurar_Click(object sender, EventArgs e)
+        {
+            lblTarefa.Text = "Tarefa: Procurar";
+            Tarefa = "P";
+            DataAgora = DateTime.Now;
+            DataEncerra = DataAgora.AddSeconds(25);  //Adiciona 25 segundos ao tempo atual        
+            timer1.Start(); //Inicia o timer 
+
+            lblEstado.Text = "Estado: Ocupado ";
+        }
+
+        //Evento do timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Verifica se a data de encerramento é maior que a hora atual
+            if(DataEncerra > DateTime.Now)
+            {
+                lblEstado.Text = "Estado: Ocupado";
+                btnAndar.Enabled = false;
+                btnProcurar.Enabled = false;
+
+            }
+            else
+            {
+                //Verifica se a Tarefa é a Andar
+                if (Tarefa.Equals("A"))
+                {
+                    timer1.Stop(); //Para o tempo do Timer
+                    btnAndar.Enabled = true;
+                    btnProcurar.Enabled = true;
+                    lblTarefa.Text = "Tarefa:";
+                    lblEstado.Text = "Estado: Pronto";
+                }
+                //Verifica se a Tarefa é a Procurar
+                if(Tarefa.Equals("P"))
+                {
+                    timer1.Stop(); //Para o tempo do Timer
+                    Random rand = new Random(); // Objeto Randomico
+                    Double valor = rand.NextDouble();  //Gera um número randômico
+
+                    //Verifica se o numero sorteado é maior que 0.5, deixando 50% de chance para maior ou menor
+                    if (valor <= 0.5) 
+                    {
+                        btnAndar.Enabled = true;
+                        btnProcurar.Enabled = true;
+                        lblTarefa.Text = "Tarefa:";
+                        lblEstado.Text = "Estado: Recuperando";
+                    }
+                    else
+                    {
+                        Extrair(); // Executa o método de extração
+                    }
+                }
+
+                if(Tarefa.Equals("E"))
+                {
+                    if (contadorExtracao < 5)
+                    {
+                        timer1.Stop(); //Para o tempo do Timer
+                        btnAndar.Enabled = true;
+                        btnProcurar.Enabled = true;
+                        lblTarefa.Text = "Tarefa:";
+                        lblEstado.Text = "Estado: Recuperando";
+                    }
+                    else 
+                    {
+                        lblTarefa.Visible = false;                        
+                        btnAndar.Enabled = false;
+                        btnProcurar.Enabled = false;
+                        lblEstado.Visible = false;
+                        lblMsgSucesso.Visible = true;
+                        
+                    }
+                }
+            }
+            
+        }
+
+        //Método que extrai o petróleo
+        private void Extrair() 
+        {
+            if(contadorExtracao < 5)
+            {
+                lblTarefa.Text = "Tarefa: Extrair";
+                Tarefa = "E";
+                DataAgora = DateTime.Now;
+                DataEncerra = DataAgora.AddSeconds(30); //Adiciona 30 segundos ao tempo atual   
+                timer1.Start();
+                contadorExtracao++; //Adiciona 1 ao contador
+                lblEstado.Text = "Estado: Ocupado ";
+            }            
+            
+        }
+        
+
+            
+    }
+}
